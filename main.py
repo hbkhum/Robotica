e13a9defa1f1abaf40816d3a4809ca43547cb221
Sergio <p.s.sergio1320@gmail.com>
2023-03-31 19:23:27 -0700

    Examen Sergio

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from flask import Flask, redirect, url_for, render_template, request, jsonify
 from pymodbus.client import ModbusTcpClient
 from os import system
+import json
 
 # from pymodbus.client import AsyncModbusSerialClient
 app = Flask(__name__)
 
 
-@app.route("/modbus/tcpip/writeHoldingRegister", methods=["POST"])
+@app.route("/modbus/tcpip/Readoutput", methods=["POST"])
 def modbus():
     request_data = request.get_json()
     ip = request_data['IpAddress']
     try:
         client = ModbusTcpClient(ip)
         client.connect()
-        client.write_register(request_data['Address'], request_data['Value'])
+        #client.write_register(128,19)
+        r = client.read_coils(136,8)
         client.close()
-        return jsonify(True), 201
+        return jsonify(r.bits), 201
     except:
         print("An exception occurred")
 
