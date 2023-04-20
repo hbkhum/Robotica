15948397d86a8324737ab10428bf26de3516ca2a
hbkhum <humberto.pedraz@gmail.com>
2023-04-20 10:57:58 -0700

    Fix Branch

--- main.py
+++ main.py
@@ -2,17 +2,10 @@ from flask import Flask, redirect, url_for, render_template, request, jsonify
 from pymodbus.client import ModbusTcpClient
 from os import system
 
-# from pymodbus.client import AsyncModbusSerialClient
 app = Flask(__name__)
 
-
-<<<<<<< HEAD
 @app.route("/modbus/tcpip/ReadInputs", methods=["POST"])
 def readInputs():
-=======
-@app.route("/modbus/tcpip/ReadHoldingRegister", methods=["POST"])
-def modbus():
->>>>>>> Cristian
     request_data = request.get_json()
     ip = request_data['IpAddress']
     pin= int(request_data['Pin'])
@@ -20,17 +13,11 @@ def modbus():
     try:
         client = ModbusTcpClient(ip)
         client.connect()
-<<<<<<< HEAD
         input = client.read_input_registers(int(request_data['Address']))
         client.close()
         port = int(input.registers[0])
         result = port & pin
         return jsonify(bool(result)), 201
-=======
-        reg = client.read_holding_registers(request_data['Address'], 1)
-        client.close()
-        return jsonify(reg.registers[0]), 201
->>>>>>> Cristian
     except:
         print("no funciona")
 
